3c8db5c1d6ecd60bdc294f081779be385ebaa609
richard <richardsifeanyi@gmail.com>
2023-03-29 06:24:41 -0700

    Uncle

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -7,11 +7,10 @@
  */
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
-	if (node == NULL || node->parent == NULL)
+	if (node == NULL || node->parent == NULL || node->parent->parent NULL)
 		return (NULL);
-	if (node->parent->right == node)
-		return (node->parent->left);
-	else
-		return (node->parent->right);
+	if (node->parent->parent->left == node->parent)
+		return (node->parent->parent->right);
+	return (node->parent->parent->left);
 
 }
